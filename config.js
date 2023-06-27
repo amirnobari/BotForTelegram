3efb4f56a5433fa288588dc95edc862e03901952
amir <amirnobari1990@gmail.com>
2023-06-27 18:48:24 +0330

    created config file

---
+++ config.js
@@ -0,0 +1,13 @@
+const session = require('telegraf-session-local')
+
+const enableSession = (bot) => {
+    bot.use(new session())
+  }
+  
+  const isChatGptEnabled = false
+  
+  module.exports = {
+    enableSession: enableSession,
+    isChatGptEnabled: isChatGptEnabled
+  }
+  
