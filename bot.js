30df0aba5e81b800b4d51a9a59bf24cde12d1bf9
amir <amirnobari1990@gmail.com>
2023-06-27 11:53:08 +0330

    add chatgpt in telegram bot

--- bot.js
+++ bot.js
@@ -1,93 +1,68 @@
-require('dotenv').config();
+const { Telegraf, Markup, session } = require('telegraf')
+const { Configuration, OpenAIApi } = require('openai')
+require('dotenv').config()
 
-const { Telegraf, Markup, session } = require('telegraf');
-const axios = require('axios');
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const configuration = new Configuration({
+  organization: "org-y0vIHonZMz14MkjwPTUH55wD",
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 // Enable session
-bot.use(session());
+bot.use(session())
 
 // Handler for the /start command
 bot.start((ctx) => {
-    const username = ctx.from.first_name;
-    ctx.reply(`Welcome To My Bot Dear, ${username}!`, Markup.inlineKeyboard([
-        [Markup.button.callback('Ask a Question', '/ask')],
-        [Markup.button.callback('About Me', 'about_me')]
-    ]));
-});
+  const username = ctx.from.username
+  ctx.reply(`Welcome To My Bot Dear, ${username}!`, Markup.inlineKeyboard([
+    [Markup.button.callback('About Me', 'about_me')],
+  ]))
+})
 
 // Middleware for showing the about me message
 bot.action('about_me', (ctx) => {
-    const linkedinUrl = 'https://www.linkedin.com/in/amir-nobari1990/';
-    const githubUrl = 'https://github.com/amirnobari';
-    const infoMessage = `
-        <b>About Me</b>
-        I am a bot developed by Amir Nobari.
-        For more information, please visit my LinkedIn profile or
-        GitHub profile.
-    `;
-    ctx.answerCbQuery(infoMessage);
-    ctx.replyWithHTML(infoMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: 'LinkedIn Profile', url: linkedinUrl },
-                    { text: 'GitHub Profile', url: githubUrl }
-                ]
-            ]
-        }
-    });
-});
-
-// Handler for the "Ask a Question" button
-bot.action('/ask', (ctx) => {
-    const askMessage = 'Please ask your question:';
-    ctx.reply(askMessage);
-
-    // Set the state to indicate that the bot is ready to receive the question
-    ctx.session.waitingForQuestion = true;
-});
+  const linkedinUrl = 'https://www.linkedin.com/in/amir-nobari1990/'
+  const githubUrl = 'https://github.com/amirnobari'
+  const infoMessage = `
+    <b>About Me</b>
+    I am a bot developed by Amir Nobari.
+    For more information, please visit my LinkedIn profile or
+    GitHub profile.
+  `
+  ctx.answerCbQuery(infoMessage)
+  ctx.replyWithHTML(infoMessage, {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'LinkedIn Profile', url: linkedinUrl },
+          { text: 'GitHub Profile', url: githubUrl },
+        ],
+      ],
+    },
+  })
+})
 
 // Handler for receiving messages
-bot.on('text', async (ctx) => {
-    const message = ctx.message.text;
+bot.on("message", async (ctx) => {
+  const question = ctx.message.text
 
-    // Check the type of the message
-    if (ctx.session.waitingForQuestion) {
-        // The bot is ready to receive the question
-        const question = message;
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: question,
+      max_tokens: 1000,
+      temperature: 0.7,
+    }).then(response => {
+      ctx.reply(response.data.choices[0].text)
+    }).catch(err => {
+      console.log(err)
+    })
+  } catch (error) {
+    console.error(error)
+    ctx.reply('Sorry, an error occurred while processing your question.')
+  }
+})
 
-        // Call the ChatGPT API to get a response
-        try {
-            const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-                question: question,
-                // Add any additional parameters required by your API
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${process.env.API_TOKEN}`,
-                    // Add any other required headers
-                },
-            });
-
-            const answer = response.data.answer;
-
-            ctx.reply(answer);
-        } catch (error) {
-            console.error(error);
-            ctx.reply('Sorry, an error occurred while processing your question.');
-        }
-
-        // Reset the state
-        ctx.session.waitingForQuestion = false;
-    } else {
-        if (message === '/cancel') {
-            const cancelMessage = 'Operation canceled.';
-            ctx.reply(cancelMessage);
-        }
-    }
-});
-
-// Start the bot
-bot.launch();
-console.log('Bot is running!');
+bot.launch()
+console.log('Bot is running!')
