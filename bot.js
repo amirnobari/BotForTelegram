00d5083dd91827a36c52c1a21aa15e8223237468
amir <amirnobari1990@gmail.com>
2023-06-28 21:35:57 +0330

    update and added new code

--- bot.js
+++ bot.js
@@ -1,33 +1,32 @@
-// bot.js
-const { Telegraf } = require('telegraf')
-const { enableSession, isChatGptEnabled } = require('./config')
-require('dotenv').config()
-const bot = new Telegraf(process.env.BOT_TOKEN)
-const startHandler = require('./startHandler')
 
-require('./buy')(bot)
+const { Telegraf } = require('telegraf');
+const { enableSession, isChatGptEnabled } = require('./config');
+require('dotenv').config();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const startHandler = require('./startHandler');
 
-const aboutMe = require('./aboutMe')
-const gptConversation = require('./gptConversation')
-const session = require('telegraf-session-local')
-const handleMessage = require('./messageHandler')
+require('./buy')(bot);
+
+const aboutMe = require('./aboutMe');
+const gptConversation = require('./gptConversation');
+const handleMessage = require('./messageHandler');
 
 // Enable session
-enableSession(bot)
+enableSession(bot);
 
 // Register start command handler from startHandler.js
-bot.start((ctx) => startHandler.handleStartCommand(ctx, bot))
+bot.start((ctx) => startHandler.handleStartCommand(ctx, bot));
 
 // Middleware for showing the about me message
-bot.action('about_me', aboutMe.showAboutMe)
+bot.action('about_me', aboutMe.showAboutMe);
 
 // Handler for enabling chat with GPT
 bot.action('ENABLE_CHATGPT', (ctx) => {
-  handleMessage.enableChatGPT(ctx)
-})
+  handleMessage.enableChatGPT(ctx);
+});
 
 // Handler for receiving messages
-bot.on('message', (ctx) => handleMessage.handleMessage(ctx, isChatGptEnabled))
+bot.on('message', (ctx) => handleMessage.handleMessage(ctx, isChatGptEnabled));
 
-bot.launch()
-console.log('Bot is running!')
+bot.launch();
+console.log('Bot is running!');
--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 const session = require('telegraf-session-local')
 
 const enableSession = (bot) => {
-    bot.use(new session())
-  }
-  
-  const isChatGptEnabled = false
-  
-  module.exports = {
-    enableSession: enableSession,
-    isChatGptEnabled: isChatGptEnabled
-  }
-  
+  bot.use(new session())
+}
+
+const isChatGptEnabled = false
+
+module.exports = {
+  enableSession: enableSession,
+  isChatGptEnabled: isChatGptEnabled
+}
