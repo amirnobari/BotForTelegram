a81acb3c8029be446745ac92b0e0243ddc442aad
amir <amirnobari1990@gmail.com>
2023-07-02 00:04:59 +0330

    adde chanel

--- start-handler/startHandler.js
+++ start-handler/startHandler.js
@@ -2,10 +2,11 @@ const { Markup } = require('telegraf')
 
 const handleStartCommand = (ctx) => {
   const username = ctx.from.username
-  ctx.reply(`کاربر ${username} به ربات دنیای برنامه نویسی خوش آمدی` , Markup.inlineKeyboard([
+  ctx.reply(`کاربر ${username} به ربات دنیای برنامه نویسی خوش آمدی`, Markup.inlineKeyboard([
     [Markup.button.callback('در مورد سازنده ربات بیشتر بدانید', 'about_me')],
     [Markup.button.callback(' منابع آموزشی برنامه نویسی', 'resources')],
-    [Markup.button.callback('وب سایت هایی برای حل خطاها در کد نویسی','error')],
+    [Markup.button.callback('کانال و گروه های برنامه نویسی', 'chanel')],
+    [Markup.button.callback('وب سایت هایی برای حل خطاها در کد نویسی', 'error')],
     [Markup.button.callback('مقالات برنامه نویسی', 'articles')],
     [Markup.button.callback('وب سایت هایی برای تمرین و یادگیری برنامه نویسی', 'practice')],
     [Markup.button.callback('وب سایت هایی برای پیدا کردن رود مپ جهت یادگیری برنامه نویسی', 'road_map')],
