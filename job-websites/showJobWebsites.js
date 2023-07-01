07d165842bdd442a2a17b106bfcbea5998d8d2ac
amir <amirnobari1990@gmail.com>
2023-07-02 00:27:46 +0330

    added new job web

--- job-websites/showJobWebsites.js
+++ job-websites/showJobWebsites.js
@@ -6,6 +6,7 @@ const showJobWebsites = (ctx) => {
     [Markup.button.url(' سایت (جابینجا) ', 'https://www.jobinja.ir/')],
     [Markup.button.url(' سایت (inded) ', 'https://www.indeed.com/')],
     [Markup.button.url(' سایت (glassdoor) ', 'https://www.glassdoor.com')],
+    [Markup.button.url('کانال (Job Finding | Search for your job)', 'https://t.me/jobs_finding')],
   ]));
 };
 
