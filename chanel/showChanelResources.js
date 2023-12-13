8c57d3cc486aa8482c9499c190bdf1312480c155
amir <amirnobari1990@gmail.com>
2023-12-13 20:01:11 +0330

    chore : "update new address"

--- chanel/showChanelResources.js
+++ chanel/showChanelResources.js
@@ -4,7 +4,7 @@ const showChanelResources = (ctx) => {
   ctx.reply('کانال و گروه های مرتبط به برنامه نویسی', Markup.inlineKeyboard([
     [Markup.button.url('کانال دورهمی برنامه نویسان', 'https://t.me/irdev2')],
     [Markup.button.url('کانال (Tech Immigrants)', 'https://t.me/techimmigrants')],
-    [Markup.button.url('کانال (Hangouts to Learn)', 'https://t.me/hangouts_tolearn')],
+    [Markup.button.url('کانال (js_challenges)', 'https://t.me/js_challenges')],
     
   ]));
 };
--- educational-resources/showJavaScriptResources.js
+++ educational-resources/showJavaScriptResources.js
@@ -2,7 +2,7 @@ const { Markup } = require('telegraf')
 
 const showJavaScriptResources = (ctx) => {
   ctx.reply('تمام دوره های رایگان جاوا اسکریپت', Markup.inlineKeyboard([
-    [Markup.button.url('(Jonas Schmedtmann) دوره کامل جاوا اسکریپت 2021 - از مبتدی تا پیشرفته زیرنویس دار', 'https://t.me/developersubtitle')],
+    [Markup.button.url('(Jonas Schmedtmann) دوره کامل جاوا اسکریپت 2021 - از مبتدی تا پیشرفته دوبله فارسی ', 'https://t.me/jschallenges_bot')],
     [Markup.button.url('آموزش مقدماتی تا پیشرفته جاوا اسکریپت (تاپلرن)', 'https://toplearn.com/c/W6J')],
     [Markup.button.url('آموزش جاوا اسکریپت (سبز لرن)', 'https://toplearn.com/c/W6J')],
     [Markup.button.url('(انگلیسی,Mosh)آموزش جاوا اسکریپت برای مبتدیان در 1 ساعت', 'https://youtu.be/W6NZfCO5SIk')],
